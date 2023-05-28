5b9e2ded9e4663f99e446f68048efdbcdbd20b95
Redfire <88282354+Reuvi@users.noreply.github.com>
2023-05-27 22:32:06 -0400

    Create image_tester.py

---
+++ image_tester.py
@@ -0,0 +1,14 @@
+import requests
+
+url = 'http://50.116.61.76/upload'  # Website IP
+
+image_path = 'image.jpg'  # Path to the image file
+
+with open(image_path, 'rb') as image_file:
+    files = {'image': image_file}
+    response = requests.post(url, files=files)
+
+if response.status_code == 200:
+    print('Image upload successful')
+else:
+    print('Error:', response.json()['error'])
